f147ecab3e3a7e6fd6c72de20b3dc3092da77c0a
Adam Kwieciński <adam.kw94@gmail.com>
2020-07-05 23:09:42 +0200

    Initial commit: basic structure

---
+++ src/com/adamkwiecinski/Player.java
@@ -0,0 +1,4 @@
+package com.adamkwiecinski;
+
+public class Player {
+}
---
+++ src/com/adamkwiecinski/TicTacToeTest.java
@@ -0,0 +1,8 @@
+package com.adamkwiecinski;
+
+public class TicTacToeTest {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
